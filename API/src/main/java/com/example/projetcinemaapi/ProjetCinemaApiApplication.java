072d9ab4dff2e35be0e2d6274242936a600645b0
Yaimuu <yanis.ouled@gmail.com>
2022-09-14 11:42:52 +0200

    API imported

---
+++ API/src/main/java/com/example/projetcinemaapi/ProjetCinemaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetcinemaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCinemaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCinemaApiApplication.class, args);
+    }
+
+}
---
+++ API/src/test/java/com/example/projetcinemaapi/ProjetCinemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetcinemaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCinemaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
