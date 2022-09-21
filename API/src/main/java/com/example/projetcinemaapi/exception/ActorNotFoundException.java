8b0ed17ff1431cd03d6eb5ba0d9cf65847f50f17
Titouan GUY <Titouan.GUY@kardol.fr>
2022-09-21 12:45:11 +0200

    base des services

---
+++ API/src/main/java/com/example/projetcinemaapi/exception/ActorNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.projetcinemaapi.exception;public class ActorNotFoundException {
+}
---
+++ API/src/main/java/com/example/projetcinemaapi/service/PersonnageService.java
@@ -0,0 +1,2 @@
+package com.example.projetcinemaapi.service;public class PersonnageService {
+}
