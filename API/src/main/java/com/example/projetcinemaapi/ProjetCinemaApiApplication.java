d0436ea5601619445545cb13cf5f739e1efe183a
Yaimuu <yanis.ouled@gmail.com>
2022-09-21 12:35:50 +0200

    fix fucking run bug

--- API/src/main/java/com/example/projetcinemaapi/ProjetCinemaApiApplication.java
+++ API/src/main/java/com/example/projetcinemaapi/ProjetCinemaApiApplication.java
@@ -2,8 +2,12 @@ package com.example.projetcinemaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories()
+@EntityScan()
 public class ProjetCinemaApiApplication {
 
     public static void main(String[] args) {
