8ddd2435d3dd85be352d2c84652d2e33484a1c65
Yaimuu <yanis.ouled@gmail.com>
2022-10-19 11:17:25 +0200

    front update

--- AngularCli/src/app/layouts/navbar/navbar.component.ts
+++ AngularCli/src/app/layouts/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AuthService} from "../../shared/auth.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.authService.doLogout()
   }
 
 }
---
+++ AngularCli/src/app/shared/auth.service.ts
@@ -0,0 +1,55 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../model/user.model";
+import {BehaviorSubject, Observable} from "rxjs";
+import {Router} from "@angular/router";
+import {environment} from "../../environments/environment";
+import {JwtResponse} from "../model/jwt-response.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  private errorMessage$ = new BehaviorSubject<string>('');
+  public getErrorMessage = this.errorMessage$.asObservable();
+
+  constructor(private http: HttpClient, public router: Router) {
+  }
+
+  signIn(user: User) {
+    return this.http
+      .post<JwtResponse>(`${environment.baseUrl}/Authentication/login`, user)
+      .subscribe(
+        {
+          next: jwt => {
+            localStorage.setItem('access_token', jwt.accessToken);
+            this.getUser(user.username!).subscribe((userRes) => {
+              localStorage.setItem('currentUser', JSON.stringify(userRes));
+              //TODO route à redéfinir, mais on redirigera pas dans la même page selon le role
+              const route = userRes.role === 'ADMIN' ? 'admin' : 'home';
+              this.router.navigate([route]);
+            });
+          }, error: err => this.errorMessage$.next(err.error.message)
+        });
+  };
+
+  public getToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  get isLoggedIn(): boolean {
+    let authToken = localStorage.getItem('access_token');
+    return authToken !== null;
+  }
+
+  public doLogout() {
+    localStorage.removeItem('access_token');
+  }
+
+  private getUser(id: string): Observable<User> {
+    let api = `${environment.baseUrl}/users/${id}`;
+    return this.http.get<User>(api);
+  }
+}
