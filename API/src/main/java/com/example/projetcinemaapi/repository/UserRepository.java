431b43884b7d50fa205094a7379e98a91adda980
brsantoine <antoinebre@orange.fr>
2022-11-22 17:51:11 +0100

    added Personnage branch

--- API/src/main/java/com/example/projetcinemaapi/repository/UserRepository.java
+++ API/src/main/java/com/example/projetcinemaapi/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.projetcinemaapi.repository;
 import com.example.projetcinemaapi.domains.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User rechercheNom(String login);
+    Optional<User> findByLogin(String login);
 }
